9fd2a16f85f43c64830fa1b551ba2256fac60b3d
Roger Holden <r.holden@chi.ac.uk>
2019-11-01 13:35:12 +0000

    changes in arrays file and addition of a conditional test.

--- basics/8-arrays.php
+++ basics/8-arrays.php
@@ -3,11 +3,31 @@
 //arrays
 $myarray = array(1,4,6,77,88, 888888);
 
-//or shorthand $array[1,4,6,77,88];
+//or shorthand $array = [1,4,6,77,88];
 
+$array = [1,4,6,77,88];
 
 echo $myarray[0];
 
+$quickarray = [     "foo" => "bar",     "bar" => "foo",];
+
+$quickarray = array(    "foo1" => "bar1",    "bar1" => "foo1",);
+
+
+$quickarray = array( );
+
+
+print_r($quickarray);
+
+ 
+$students = explode(" ", "Sam Max Mary George Ben Tim");
+
+print_r($students);
+
+
+
+
+exit;
 
 
 // foreach ($myarray as $value) {
@@ -16,9 +36,6 @@ echo $myarray[0];
 //
 //
 // }
-
-
-
 
 
 
@@ -38,7 +55,7 @@ echo $myarray[0];
 
  foreach ($user_details_assoc as $key => $value) {
 
-     echo $key . " = ".$value . "<br>";
+     if (!is_array($value)) echo $key . " = ".$value . "<br>";
 
 
  }
---
+++ basics/conditionals/if.php
@@ -0,0 +1,13 @@
+<?php
+
+$gender = 'male';
+$age = 17;
+
+
+if ($gender == 'male' || $age >= 18  ) echo "You can enter";
+
+else echo "You can't come in";
+
+
+
+?>
