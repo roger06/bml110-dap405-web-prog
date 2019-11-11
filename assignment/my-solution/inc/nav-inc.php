ea61d783c3d74da5743b884721706c15dafef2ff
Roger Holden <r.holden@chi.ac.uk>
2019-11-11 17:40:38 +0000

    Tax table display script added though not working properly

--- assignment/my-solution/inc/nav-inc.php
+++ assignment/my-solution/inc/nav-inc.php
@@ -13,27 +13,9 @@
             <ul class="nav navbar-nav">
                 <li class="active"><a href="#"><span class="glyphicon glyphicon-home"></span>About</a></li>
                 <li><a href="#"><span class="glyphicon glyphicon-calendar"></span>Payroll Dates</a></li>
-                
-                <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown"><span
-                    class="glyphicon glyphicon-search"></span>Search <b class="caret"></b></a>
-                    <ul class="dropdown-menu" style="min-width: 300px;">
-                        <li>
-                            <div class="row">
-                                <div class="col-md-12">
-                                    <form class="navbar-form navbar-left" role="search">
-                                    <div class="input-group">
-                                        <input type="text" class="form-control" placeholder="Search" />
-                                        <span class="input-group-btn">
-                                            <button class="btn btn-primary" type="button">
-                                                Go!</button>
-                                        </span>
-                                    </div>
-                                    </form>
-                                </div>
-                            </div>
-                        </li>
-                    </ul>
-                </li>
+                <li><a href="taxbands.php"><span class="glyphicon glyphicon-list-alt"></span>Tax Bands</a></li>
+
+               
             </ul>
             <ul class="nav navbar-nav navbar-right">
                 <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown"><span
---
+++ assignment/my-solution/taxbands.php
@@ -0,0 +1,149 @@
+<?php 
+
+require('inc/fns-inc.php');
+require_once('inc/header-inc.php');
+
+$taxfile   = 'data/tax-tables.json';
+
+try {
+    
+    // $json = @file_get_contents($jsonfile) or die("cannot open file - $jsonfile");
+    $taxjson = @file_get_contents($taxfile); 
+} catch (Exception $e) {
+
+   // echo 'Caught exception: ',  $e->getMessage(), "\n";
+
+    include('inc/error-inc.php');
+    include('inc/footer-inc.php');
+    exit;
+}
+
+restore_error_handler();
+// https://www.php.net/manual/en/function.restore-error-handler.php
+
+
+$tax_rates_array = json_decode($taxjson, true); 
+
+
+$header_array = array("name"=>"Name", "description"=>"Description", "minsalary"=>"Min Salary", "maxsalary"=>"Max Salary","rate"=>"Rate", "exceptions"=>"Exceptions");
+// var_dump( json_decode($json, false));
+
+?>
+
+<main class="container">
+<h1>Tax Rates <?php echo date("Y");?></h1>
+<div class="card">
+    <div class="table-responsive">
+        <table class="table table-striped table-bordered table-hover">
+        <tr>
+<?php
+
+// echo "<pre>"; print_r($tax_rates_array); echo "</pre>";
+
+// print header row
+foreach ($header_array as $header => $label){
+    echo "<th>" . $label . "</th>";
+}
+?>
+
+</tr>
+
+
+<?php
+ foreach($tax_rates_array as $data){
+    echo "<tr>";  
+    foreach ($header_array as $header => $label){
+        
+
+        // handle the exceptions field - this is probably much easier as an array than object!
+        if (is_array($data[$header])) {
+            echo "data=>$header is an array!!!";
+            // $arr = $data->$header;
+            // // $data->$header = implode( ",", $arr);
+            // $implode = implode( ",", $data[$header]);
+            $implode = '';
+
+            foreach ($data[$header] as $key => $value) {
+
+                foreach ($value as $key => $data)
+                $implode .= $key . " " .$data;
+                $implode .= ", ";
+
+            }
+
+            echo "Implode = " .$implode;
+            $header = $implode;
+            // echo "<pre>"; print_r($data[$header]); echo "</pre>";
+
+        }
+
+        echo "<td>" . $data[$header] . "</td>";
+        }
+    
+   
+
+echo "</tr>";
+} // end outer foreach
+
+
+exit;
+
+
+
+foreach($tax_rates_array as $data){
+    // var_dump( $data);
+
+
+    echo "<tr>";
+
+
+        echo write_cell($data->name);
+        // echo write_cell($data->firstname);
+        // echo write_cell($data->lastname);
+        // echo write_cell($data->jobtitle);
+        // echo write_cell($data->salary, "GBP");
+
+        // echo write_cell($band);
+        
+        
+        // echo write_cell( calcTax($data->salary, $band));
+
+
+    
+    echo "</tr>";
+
+}
+?>
+        </table>
+    </div>
+</div>
+
+<?php
+exit;
+
+if  ( is_object($emp_json_data)    ) echo 'json_data is object<br>';
+else echo 'json_data is not object<br>';
+
+
+if  ( is_array($emp_json_data)    ) echo 'json_data is array<br>';
+else echo 'json_data is not array<br>';
+
+exit;
+echo "<pre>";
+
+    print_r($emp_json_data);
+echo "</pre>";
+
+?>
+
+</main>
+
+
+ 
+
+
+
+
+
+
+<?php require_once('inc/footer-inc.php');?>
