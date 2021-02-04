3639490eb103d88c78d784bd58b54ad8e5c635e5
Roger Holden <r.holden@chi.ac.uk>
2021-02-04 17:27:32 +0000

    week one added

---
+++ Week 1/hello-holding.php
@@ -0,0 +1,13 @@
+ 
+<?php
+
+
+    echo "hello, this is PHP";
+
+    // now do something HTML can't
+
+    // echo "the date today is ";
+
+    // echo date("d-m-y");
+
+?>
---
+++ Week 1/hello.php
@@ -0,0 +1,20 @@
+ 
+ 
+
+<?php
+
+
+    echo "<h1></h1>hello, this is PHP...</h1>";
+// now do something HTML can't
+
+    // echo "the date today is ";
+
+ 
+
+     echo date("d-m-y"); 
+
+?>
+
+
+
+ 
